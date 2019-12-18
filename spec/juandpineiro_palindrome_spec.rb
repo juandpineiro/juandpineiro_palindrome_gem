6ba0c5b03ef936e3a67f77f9761febbf0f1060ec
Juan Piñeiro <juandpineiro@gmail.com>
2019-12-18 11:09:27 -0400

    Add full RSpec test suite

--- spec/juandpineiro_palindrome_spec.rb
+++ spec/juandpineiro_palindrome_spec.rb
@@ -1,13 +1,41 @@
 require 'juandpineiro_palindrome'
 
-RSpec.describe JuandpineiroPalindrome do
+describe JuandpineiroPalindrome do
   describe '#palindrome?' do
-    it 'returns false given a non-palindrome' do
-      expect('apple'.palindrome?).to be false
+    context 'returns true' do
+      it 'given a string palindrome' do
+        expect('racecar'.palindrome?).to be true
+      end
+
+      it 'given a mixed-case string palindrome' do
+        expect('RaceCar'.palindrome?).to be true
+      end
+
+      it 'given a string palindrome with punctuation' do
+        expect("Madam, I'm Adam.".palindrome?).to be true
+      end
+
+      it 'given an integer palindrome' do
+        expect(12321.palindrome?).to be true
+      end
     end
 
-    it 'returns true given a literal palindrome' do
-      expect('racecar'.palindrome?).to be true
+    context 'returns false' do
+      it 'given a string non-palindrome' do
+        expect('apple'.palindrome?).to be false
+      end
+
+      it 'given an empty string' do
+        expect(''.palindrome?).to be false
+      end
+
+      it 'given a whitespace string' do
+        expect("  \n  \t  \r  ".palindrome?).to be false
+      end
+
+      it 'given an integer non-palindrome' do
+        expect(12345.palindrome?).to be false
+      end
     end
   end
 end
